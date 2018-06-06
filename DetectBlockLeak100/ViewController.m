4b2e24e8d8bceb7ad907c6e5a1bb4ef0ced1ecc6
Anthony Maxwell <nymaxa0803@yahoo.com>
2018-06-06 13:39:24 -0400

    Added the function, and its corresponding callBack method. No memory leak has been introduced yet.

--- DetectBlockLeak100/ViewController.m
+++ DetectBlockLeak100/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self CreateLeak];
 }
 
 
@@ -24,6 +25,20 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(void) CreateLeak{
+    NSLog(@"\ncreate a block that will cause a memory leak\n");
+    
+    [self letsDoIt:^{
+        NSLog(@"\ncallBack block was called.\nPlace all of your callBack code here. It will run after the letsDoIt main body call it.\n");
+    }];
+}
 
+-(void) letsDoIt:(void (^)(void))callBack
+{
+    NSLog(@"\n (void) letsDoIt:(void (^)(void))callBack method's code body goes here\n...calling the callBack block when this method's body is finished running its code.\n");
+   // [self performSelector:@selector(delayedMethod) withObject:nil afterDelay:3.5];
+    callBack();
+    
+}
 
 @end
